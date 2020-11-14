74372476726a61e62baf90de3cc14935b8799122
Adrien SCHAEFERS <aschaefers@aschaefers.home>
2020-11-14 16:39:00 +0100

    Ajout du texte à côté de la souris + initialisation

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const cursor = document.querySelector("div.cursor")
+const canvasTag = document.querySelector("canvas.in")
+
 
 // when I clicked down on page I want the curosr to get bigger
 const growCursor = function () {
     cursor.classList.add("is-down")
+    cursor.innerHTML = `<span>Let's get some fun !</span>`
 }
 
 // When I clicked up I want the cursor the get smaller
 const shrinkCursor = function () {
     cursor.classList.remove("is-down")
+    cursor.innerHTML = `<span>Click me !</span>`
 }
 
 //Move the cursor based on coordinates 
@@ -16,6 +20,21 @@ const moveCursor = function(x, y) {
     cursor.style.left = x + "px"
     cursor.style.top = y + "px"
 }
+
+//Set up a canvas 
+const setupCanvas = function (canvas){
+    const w = window.innerWidth
+    const h = window.innerHeight
+    const dpi = window.devicePixelRatio
+
+    canvas.width = w * dpi 
+    canvas.height = h * dpi
+    canvas.style.width = w + "px"
+    canvas.style.height = h + "px"
+}
+
+
+setupCanvas(canvasTag)
 
 
 document.addEventListener("mousedown", function (){
@@ -32,3 +51,6 @@ document.addEventListener("mousemove", function (event) {
 
   //event.pageX + event.pageY
 })
+
+
+
