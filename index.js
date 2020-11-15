cea5ef4db140c34646e6afe62941fdc8c2c13e03
Adrien SCHAEFERS <aschaefers@aschaefers.home>
2020-11-15 11:32:31 +0100

    essiner une ligne sur le canvas + dessiner que lorsqu'on clic

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cursor = document.querySelector("div.cursor")
 const canvasTag = document.querySelector("canvas.in")
+let isMouseDown = false
 
 
 // when I clicked down on page I want the curosr to get bigger
@@ -15,7 +16,6 @@ const shrinkCursor = function () {
 }
 
 //Move the cursor based on coordinates 
-
 const moveCursor = function(x, y) {
     cursor.style.left = x + "px"
     cursor.style.top = y + "px"
@@ -27,29 +27,62 @@ const setupCanvas = function (canvas){
     const h = window.innerHeight
     const dpi = window.devicePixelRatio
 
+    // IcI on paramètre le canvas
     canvas.width = w * dpi 
     canvas.height = h * dpi
     canvas.style.width = w + "px"
     canvas.style.height = h + "px"
+
+    //Quel est le type de rendu que je souhaite avoir ? 2D,3D?
+    const context = canvas.getContext("2d")
+
+    //Ici on paramètre le contenu qu'on veut faire apparaître dans le canvas
+    context.fillStyle  = ("red")
+    context.strokeStyle = ("red")
+    context.lineWidth = 80
+    context.lineCap = "round"
+    context.lineJoin ="round"
+    context.scale (dpi, dpi) //indispensable si c'est un écran rétina
+}
+
+
+//Lets start to draw based on the canvas and x and Y
+const startDraw = function (canvas, x , y){
+    const context = canvas.getContext("2d")
+    context.moveTo(x,y)
+}
+
+
+// Let's draw based on three things
+const moveDraw = function (canvas, x, y){
+    const context = canvas.getContext("2d")
+
+    if (isMouseDown) {
+        context.lineTo(x , y)
+        context.stroke()
+    } 
 }
 
 
 setupCanvas(canvasTag)
 
 
-document.addEventListener("mousedown", function (){
+
+document.addEventListener("mousedown", function (event){
+    isMouseDown = true
     growCursor()
+    startDraw(canvasTag, event.pageX, event.pageY)
 })
 
 document.addEventListener("mouseup", function (){
+    isMouseDown = false
     shrinkCursor()
 })
 
 document.addEventListener("mousemove", function (event) {
     moveCursor(event.pageX, event.pageY)
-
-
   //event.pageX + event.pageY
+    moveDraw(canvasTag, event.pageX, event.pageY)
 })
 
 
